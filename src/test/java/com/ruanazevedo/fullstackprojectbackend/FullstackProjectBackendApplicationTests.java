7447584b87dc7fed3f1b997ebc1aa944b836632b
ruanazevedo <ruannzeroh@gmail.com>
2023-02-10 16:50:56 -0300

    start project

---
+++ src/test/java/com/ruanazevedo/fullstackprojectbackend/FullstackProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruanazevedo.fullstackprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
