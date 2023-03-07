3d8ce6e2a1c92b48c6e4f83543de8854b469e962
ruanazevedo <ruannzeroh@gmail.com>
2023-03-07 15:39:38 -0300

    inserindo nova categoria com POST

--- src/main/java/com/ruanazevedo/fullstackprojectbackend/services/CategoryService.java
+++ src/main/java/com/ruanazevedo/fullstackprojectbackend/services/CategoryService.java
@@ -25,4 +25,9 @@ public class CategoryService {
 		Optional<Category> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName()));
 	}
+	
+	public Category insert(Category obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
