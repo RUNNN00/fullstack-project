a208ccafd7bcaab140fb5124bcca1a4faa92ebae
ruanazevedo <ruannzeroh@gmail.com>
2023-03-23 10:28:16 -0300

    personalizando o retorna da validacao

--- src/main/java/com/ruanazevedo/fullstackprojectbackend/resources/CategoryResource.java
+++ src/main/java/com/ruanazevedo/fullstackprojectbackend/resources/CategoryResource.java
@@ -8,15 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ruanazevedo.fullstackprojectbackend.domain.Category;
@@ -37,7 +29,7 @@ public class CategoryResource {
 		return ResponseEntity.ok(listDto);
 	}
 	
-	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	@GetMapping("/page")
 	public ResponseEntity<Page<CategoryDTO>> findPage(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
@@ -54,7 +46,7 @@ public class CategoryResource {
 		return ResponseEntity.ok(obj);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
+	@PostMapping
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoryDTO objDto) {
 		Category obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
